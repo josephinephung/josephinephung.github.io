d52b940224121e9096e2701eb945eb2a9cec2eeb
josephinephung <78751160+josephinephung@users.noreply.github.com>
2021-03-21 20:46:39 -0400

    Delete player.js

--- player.js
+++
@@ -1,9 +0,0 @@
-let player;
-
-function Player(classType, level, health, attack, speed) {
-this.classType = classType;
-this.level = level;
-this.health = health;
-this.attack = attack; 
-this.speed = speed;
-}
