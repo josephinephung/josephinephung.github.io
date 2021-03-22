449cc9d0c4d79940ca2ebf31fb5f94bdf020d883
josephinephung <78751160+josephinephung@users.noreply.github.com>
2021-03-21 21:27:28 -0400

    Add files via upload

---
+++ enemy.js
@@ -0,0 +1,9 @@
+let food;
+
+function Food(foodType, level, health, attack, speed) {
+this.classType = foodType;
+this.level = level;
+this.health = health;
+this.attack = attack; 
+this.speed = speed;
+}
