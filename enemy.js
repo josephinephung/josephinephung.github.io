cc368489c43c2a103cd1a79c3f355e726f3984f6
josephinephung <78751160+josephinephung@users.noreply.github.com>
2021-03-21 23:58:45 -0400

    Delete enemy.js

--- enemy.js
+++
@@ -1,9 +0,0 @@
-let food;
-
-function Food(foodType, level, health, attack, speed) {
-this.classType = foodType;
-this.level = level;
-this.health = health;
-this.attack = attack; 
-this.speed = speed;
-}
