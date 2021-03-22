a9b56b98bbab5c1b6be5eaf870cddbd23d1c6ca0
josephinephung <78751160+josephinephung@users.noreply.github.com>
2021-03-21 20:37:21 -0400

    Midterm
    JS game "Hungry Times"

---
+++ player.js
@@ -0,0 +1,9 @@
+let player;
+
+function Player(classType, level, health, attack, speed) {
+this.classType = classType;
+this.level = level;
+this.health = health;
+this.attack = attack; 
+this.speed = speed;
+}
